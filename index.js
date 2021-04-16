a49613acafad2d408099b8a5e34c9dd882c200b7
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-16 20:14:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
 
-
+function wakeDog (dogName="Byron", dogBreed="poodle") {
+  console.log(`Wake ${dogName} the ${dogBreed}`);
+  return `Wake ${dogName} the ${dogBreed}`
 }
 
+function leashDog (dogName="Byron", dogBreed="poodle") {
+  console.log(`Leash ${dogName} the ${dogBreed}`);
+  return `Leash ${dogName} the ${dogBreed}`
+}
+
+function walkToPark (dogName="Byron", dogBreed="poodle") {
+  console.log(`Walk to the park with ${dogName} the ${dogBreed}`);
+  return `Walk to the park with ${dogName} the ${dogBreed}`
+}
+
+ function throwFrisbee (dogName="Byron", dogBreed="poodle") {
+  console.log(`Throw the frisbee for ${dogName} the ${dogBreed}`);
+  return `Throw the frisbee for ${dogName} the ${dogBreed}`
+}
+
+function walkHome (dogName="Byron", dogBreed="poodle") {
+  console.log(`Walk home with ${dogName} the ${dogBreed}`);
+  return `Walk home with ${dogName} the ${dogBreed}`
+}
+
+
 var routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
-function exerciceDog(dogName,dogBreed){
-  for (let i=0,i<routine.length,i++){
+function exerciceDog(dogName,dogBreed)
+  
   
   
     
-  }
+  
   
 }

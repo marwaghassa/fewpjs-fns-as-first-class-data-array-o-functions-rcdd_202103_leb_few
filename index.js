5582b800e2206749db72b6bcf96813a0403bba08
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-16 20:11:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,5 @@
 
-function wakeDog(dogName,dogBreed) {
-  console.log("Wake Byron the poodle");
-}
-function leashDog(dogName,dogBreed) {
-  console.log("Leash Byron the poodle");
-}
-function walkToPark(dogName,dogBreed) {
-  console.log("Walk to the park with Byron the poodle");
-}
-function throwFrisbee(dogName,dogBreed) {
-  console.log("Throw the frisbee for Byron the poodle");
-}
-function walkHome(dogName,dogBreed) {
-  console.log("Walk home with Byron the poodle");
-}
-function unleashDog(dogName,dogBreed) {
-  console.log("Unleash Byron the poodle");
+
 }
 
 var routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];

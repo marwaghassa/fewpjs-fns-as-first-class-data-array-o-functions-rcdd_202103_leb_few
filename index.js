3b3e9eadcdc8674666a19facb3ef72147f9b4316
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-16 19:57:41 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function wakeDog() {
+  console.log("Wake Byron the poodle");
+}
+function wakeDog() {
+  console.log("");
+}
+function wakeDog() {
+  console.log("Wake Byron the poodle");
+}
+function wakeDog() {
+  console.log("Wake Byron the poodle");
+}
+function wakeDog() {
+  console.log("Wake Byron the poodle");
+}
+function wakeDog() {
+  console.log("Wake Byron the poodle");
+}

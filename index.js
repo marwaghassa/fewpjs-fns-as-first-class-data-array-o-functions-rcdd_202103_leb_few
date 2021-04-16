41bbf5344ba80735420625c8b221ba2345789bfa
Marwa Ghassa <marwaghassa7@gmail.com>
2021-04-16 20:06:07 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,6 +21,8 @@ function unleashDog(dogName,dogBreed) {
 var routine=[wakeDog,leashDog,walkToPark,throwFrisbee,walkHome,unleashDog];
 function exerciceDog(dogName,dogBreed){
   for (let i=0,i<routine.length,i++){
+  
+  
     
   }
   
